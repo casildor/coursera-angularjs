249816d4f7257a24f630255b00563ff0dda02384
Casildo Romero <casildor@hotmail.com.(none)>
2017-01-01 15:21:04 -0800

    Completed Assignment 1

--- assignment1_complete/app.js
+++ assignment1_complete/app.js
@@ -7,37 +7,36 @@ angular.module('CRApp1', [])
 Assignment1Controller.$inject = ['$scope', '$filter'];
 
 function Assignment1Controller($scope, $filter) {
-  $scope.lunchDishes = '';
-  $scope.lunchDishesMessage = '';
+	$scope.lunchDishes = '';
+	$scope.lunchDishesMessage = '';
 
-  $scope.checkLunchDishes = function () {
-    if($scope.lunchDishes != '' && $scope.lunchDishes != ""){
-      var dishes = ($scope.lunchDishes).split(',');
+	$scope.checkLunchDishes = function () {
+		if($scope.lunchDishes != ''){
+			var dishes = ($scope.lunchDishes).split(',');
 
-      if(dishes != null && dishes.length > 0){
-        console.log('nDishes Before='+dishes.length);
-        //remove empty white space dishes
-        for(var i = dishes.length - 1; i >= 0; i--) {
-            if(dishes[i].trim() == '') {
-               dishes.splice(i, 1);
-            }
-        }
-        console.log('nDishes After='+dishes.length);
-        console.log('--------------------');
-        if(dishes.length == 0)
-      		$scope.lunchDishesMessage = 'Please enter data first.';
-        else if(dishes.length > 0 && dishes.length <= 3)
-          $scope.lunchDishesMessage = 'Enjoy!';
-        else
-          $scope.lunchDishesMessage = 'Too much!';
-      }else{
-          $scope.lunchDishesMessage = 'Error with list of dishes. Try again.';
-      }
-    }else{
-      $scope.lunchDishesMessage = 'Please enter data first.';
-    }
-  };
-
+			if(dishes != null && dishes.length > 0){
+				console.log('nDishes Before='+dishes.length);
+				//remove empty white space dishes
+				for(var i = dishes.length - 1; i >= 0; i--) {
+					if(dishes[i].trim() == '') {
+						dishes.splice(i, 1);
+					}
+				}
+				console.log('nDishes After='+dishes.length);
+				console.log('--------------------');
+				if(dishes.length == 0)
+					$scope.lunchDishesMessage = 'Please enter data first.';
+				else if(dishes.length > 0 && dishes.length <= 3)
+					$scope.lunchDishesMessage = 'Enjoy!';
+				else
+					$scope.lunchDishesMessage = 'Too much!';
+			}else{
+				$scope.lunchDishesMessage = 'Error with list of dishes. Try again.';
+			}
+		}else{
+			$scope.lunchDishesMessage = 'Please enter data first.';
+		}
+	};
 }
 
 })();
